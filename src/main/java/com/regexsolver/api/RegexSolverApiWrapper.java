57faf85cb8d7631ebe5702c14335a06278874d12
Alexandre van Beurden <alexandre.vanbeurden78@gmail.com>
2024-08-08 19:47:41 +0200

    Set correct UA

--- src/main/java/com/regexsolver/api/RegexSolverApiWrapper.java
+++ src/main/java/com/regexsolver/api/RegexSolverApiWrapper.java
@@ -27,7 +27,7 @@ final class RegexSolverApiWrapper {
 
     private final static String DEFAULT_BASE_URL = "https://api.regexsolver.com/";
 
-    private final static String USER_AGENT = "RegexSolver Java / 1.0.3";
+    private final static String USER_AGENT = "RegexSolver Java / 1.0.2";
 
     private RegexApi api;
 
